fb719f8fd90b99f48fb48f9af22d2194bac7438e
Robbie Hirsch <4796485+robbiehirsch@users.noreply.github.com>
2023-08-05 10:28:22 -0700

    cleaned up position code

--- src/lib/getLocation.ts
+++ src/lib/getLocation.ts
@@ -1,12 +1,5 @@
 export async function getLocation() {
-
-    // navigator.geolocation.getCurrentPosition((pos) => {
-    //     console.log(`${pos.coords.longitude} ${pos.coords.latitude}`);
-    // }, () => {
-    //     return "Unable to retrieve your location";
-    // });
-
-    const pos: {coords: any} = await new Promise((resolve, reject) => {
+    const pos: any = await new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject);
     });
 
