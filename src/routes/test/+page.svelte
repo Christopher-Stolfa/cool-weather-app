dfc4b2914a9fd3f223cc30f225ed238e66af7d2a
Chris <xcstolfax@gmail.com>
2023-08-05 09:02:14 -0700

    checkout from master

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Test</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Test</h1>
+
+	<p>this is a test</p>
+
+	<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati vel id hic eius assumenda reprehenderit cumque. Provident inventore dolores aspernatur blanditiis, culpa cum, quisquam, odit porro voluptatum et ipsam repellendus?</p>
+</div>
