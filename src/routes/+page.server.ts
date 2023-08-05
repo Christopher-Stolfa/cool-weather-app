bbca7e67fa54fcf6d458bdeb06a969803fcfac33
Chris <xcstolfax@gmail.com>
2023-08-05 11:50:50 -0700

    Get the weather and render it on the page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,3 +1,10 @@
-import { env } from '$env/dynamic/private';
+import { getLocation } from "$lib/getLocation";
+import getTodaysWeather from "$lib/services/getTodaysWeather";
+import type { PageLoad } from "./$types";
 
-console.log(env.SECRET_API_KEY);
+export const load = async ({ fetch, params }) => {
+  //   const location = getLocation();
+  const res = await getTodaysWeather();
+  console.log(res);
+  return { weatherData: res };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,63 +1,64 @@
 <script>
-    import welcome from "$lib/images/svelte-welcome.webp";
-    import welcome_fallback from "$lib/images/svelte-welcome.png";
-    import { getLocation } from "$lib/getLocation";
+  import welcome from "$lib/images/svelte-welcome.webp";
+  import welcome_fallback from "$lib/images/svelte-welcome.png";
+  import { getLocation } from "$lib/getLocation";
+  import { page } from "$app/stores";
 
-    const locationPromise = getLocation();
+  const locationPromise = getLocation();
+  console.log($page);
 </script>
 
 <svelte:head>
-    <title>Home</title>
-    <meta name="description" content="Svelte demo app" />
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-    <h1>
-        <span class="welcome">
-            <picture>
-                <source srcset={welcome} type="image/webp" />
-                <img src={welcome_fallback} alt="Welcome" />
-            </picture>
-        </span>
+  <h1>
+    <span class="welcome">
+      <picture>
+        <source srcset={welcome} type="image/webp" />
+        <img src={welcome_fallback} alt="Welcome" />
+      </picture>
+    </span>
 
-        {#await locationPromise}
-            Waiting for geolocation information.
-        {:then locationResult}
-            {locationResult.long}
-            {locationResult.lat}
-        {:catch}
-            <p style="color: red">No geolocation information available.</p>
-        {/await}
-    </h1>
-
+    {#await locationPromise}
+      Waiting for geolocation information.
+    {:then locationResult}
+      {locationResult.long}
+      {locationResult.lat}
+    {:catch}
+      <p style="color: red">No geolocation information available.</p>
+    {/await}
+  </h1>
 </section>
 
 <style>
-    section {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        flex: 0.6;
-    }
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 0.6;
+  }
 
-    h1 {
-        width: 100%;
-    }
+  h1 {
+    width: 100%;
+  }
 
-    .welcome {
-        display: block;
-        position: relative;
-        width: 100%;
-        height: 0;
-        padding: 0 0 calc(100% * 495 / 2048) 0;
-    }
+  .welcome {
+    display: block;
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding: 0 0 calc(100% * 495 / 2048) 0;
+  }
 
-    .welcome img {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        display: block;
-    }
+  .welcome img {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    display: block;
+  }
 </style>
